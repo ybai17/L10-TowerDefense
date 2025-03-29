b239afe8a47ede12966cefe774717070b56c2ca6
Yilei Bai <marbink100@gmail.com>
2025-03-28 21:11:32 -0400

    added more sophisticated and organized spawn wave management functionality

--- Assets/_TowerDefenseGame/Scripts/WaveSpawner.cs
+++ Assets/_TowerDefenseGame/Scripts/WaveSpawner.cs
@@ -6,36 +6,61 @@ public class WaveSpawner : MonoBehaviour
     [System.Serializable]
     public class Wave
     {
-        public GameObject[] enemyPrefab;
+        public GameObject[] enemyPrefabs;
         public int enemyCount = 5;
         public float spawnInterval = 2f;
     }
 
     public Wave[] waves;
+    public float timeBetweenWaves = 5f;
+
+    public int currentWaveIndex = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //StartCoroutine(SpawnWave());
+        //StartCoroutine(SpawnWave(waves[0]));
+        StartCoroutine(SpawnMultipleWaves());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnMultipleWaves()
     {
-        
-    }
-
-    /* IEnumerator SpawnWave()
-    {
-        for (int i = 0; i < enemyCount; i++)
+        while (currentWaveIndex < waves.Length)
         {
-            SpawnEnemy(enemyPrefab[0]);
-            yield return new WaitForSeconds(spawnInterval);
+            Debug.Log("Wave incoming: " + (currentWaveIndex + 1));
+            yield return new WaitForSeconds(timeBetweenWaves);
+
+            Debug.Log("Spawning wave " + (currentWaveIndex + 1));
+            yield return StartCoroutine(SpawnWave(waves[currentWaveIndex]));
+
+            Debug.Log("Waiting for all enemies to die");
+            //yield return new WaitUntil(AreAllEnemiesDestroyed);
+            yield return new WaitUntil(() => GameObject.FindGameObjectsWithTag("Enemy").Length == 0);
+
+            //lambda function...reminds me of anonymous functions in javascript
+            
+            currentWaveIndex++;
         }
-    } */
+    }
+
+    IEnumerator SpawnWave(Wave wave)
+    {
+        for (int i = 0; i < wave.enemyCount; i++)
+        {
+            int enemyIndex = Random.Range(0, wave.enemyPrefabs.Length);
+            GameObject enemyPrefab = wave.enemyPrefabs[enemyIndex];
+            SpawnEnemy(enemyPrefab);
+            yield return new WaitForSeconds(wave.spawnInterval);
+        }
+    }
 
     void SpawnEnemy(GameObject enemyPrefab)
     {
         Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+
+    bool AreAllEnemiesDestroyed()
+    {
+        return GameObject.FindGameObjectsWithTag("Enemy").Length == 0;
     }
 }
